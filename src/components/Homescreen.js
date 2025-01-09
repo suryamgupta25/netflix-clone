9d01a957e8d5bcb30e9fd8b8104d092bccfc2679
suryamgupta25 <suryamg13@gmail.com>
2025-01-08 19:13:51 -0500

    added components

---
+++ src/components/Homescreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Homescreen.css";
+import Nav from './Nav.js';
+
+function HomeScreen(){
+    return (
+        <div className='homeScreen'>
+            <Nav/>
+        </div>
+    )
+}
+
+export default HomeScreen
---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Nav.css';
+
+function Nav(){
+    return (
+        <div className='nav'>
+            <img className='logo' src='https://filmfervor.com/wp-content/uploads/2022/07/Netflix-logo-red-black-png.png' alt=''></img>
+        </div>
+    )
+}
+
+export default Nav;
