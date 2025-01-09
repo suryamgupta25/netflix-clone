dcc604d799c4753fc301ec177aed0e7c0b7baeec
suryamgupta25 <suryamg13@gmail.com>
2025-01-08 21:36:59 -0500

    fixed background

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,8 +4,10 @@ import './Nav.css';
 function Nav(){
     return (
         <div className='nav'>
-            <img className='nav_logo' src='https://filmfervor.com/wp-content/uploads/2022/07/Netflix-logo-red-black-png.png' alt=''></img>
-            <img className='nav_avatar' src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png' alt=''></img>
+            <div className='nav_contents'>
+                <img className='nav_logo' src='https://images.ctfassets.net/y2ske730sjqp/6bhPChRFLRxc17sR8jgKbe/6fa1c6e6f37acdc97ff635cf16ba6fb3/Logos-Readability-Netflix-logo.png' alt=''></img>
+                <img className='nav_avatar' src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png' alt=''></img>
+            </div>
         </div>
     )
 }
